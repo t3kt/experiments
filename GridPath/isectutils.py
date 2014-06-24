9d427ea078bc851486caa92ea883abc020176f15
t3kt <tommy.etkin@gmail.com>
2014-06-24 02:24:00 -0400

    some gridpath python cleanup

--- GridPath/isectutils.py
+++ GridPath/isectutils.py
@@ -1,3 +1,4 @@
+
 def getLabel(ptnum, tbl):
 	ptrow = int(ptnum) + 1
 	parts = [
@@ -8,17 +9,44 @@ def getLabel(ptnum, tbl):
 
 def buildIntersectionTable(dat, grp, scaling):
 	dat.clear()
-	dat.appendRow(["name", "path", "rawx", "rawy", "rawz", "gridx", "gridy", "gridz"])
+	dat.appendRow(["name", "gridx", "gridy", "gridz", "rawx", "rawy", "rawz", "path"])
 	for obj in grp.children:
 		if obj.type == 'null':
 			dat.appendRow([
 				obj.name,
-				obj.path,
+				int(obj.par.tx / scaling),
+				int(obj.par.ty / scaling),
+				int(obj.par.tz / scaling),
 				obj.par.tx,
 				obj.par.ty,
 				obj.par.tz,
-				int(obj.par.tx / scaling),
-				int(obj.par.ty / scaling),
-				int(obj.par.tz / scaling)
+				obj.path
 			])
 
+def findNeighbors(inters, i):
+	ix, iy, iz = inters[i, "gridx"], inters[i, "gridy"], inters[i, "gridz"]
+	for nx in (-1, 0, 1):
+		for ny in (-1, 0, 1):
+			for nz in (-1, 0, 1):
+				if nx == 0 and ny == 0 and nz == 0:
+					continue
+				ni = findIntersection(inters, (ix + nx, iy + ny, iz + nz))
+				if ni is not None:
+					yield ni
+
+def findIntersection(inters, pos):
+	for i in range(1, inters.numRows):
+		if not inters[i, "gridx"].val == str(pos[0]):
+			continue
+		if not inters[i, "gridy"].val == str(pos[1]):
+			continue
+		if not inters[i, "gridz"].val == str(pos[2]):
+			continue
+		return i
+
+def buildConnectionTable(dat, inters):
+	dat.clear()
+	dat.appendRow(['source', 'target'])
+	for i in range(1, inters.numRows):
+		for ni in findNeighbors(inters, i):
+			dat.appendRow([inters[i, "name"], inters[ni, "name"]])
