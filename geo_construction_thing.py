8a4e4b1b04ef9c3264c524d6a6967b568382659e
tekt <tekt@optexture.com>
2018-11-03 18:48:12 -0800

    geo construction with rhombuses

--- geo_construction_thing.py
+++ geo_construction_thing.py
@@ -109,3 +109,102 @@ class sidepattern:
 			sides.append(side % polysides)
 		return sides
 
+
+def AddQuad(sop, pos0, pos1, pos2, pos3):
+	poly = sop.appendPoly(4, closed=True, addPoints=True)
+	polypoints = [v.point for v in poly]
+	_setpointpos(polypoints[0], pos0)
+	_setpointpos(polypoints[1], pos1)
+	_setpointpos(polypoints[2], pos2)
+	_setpointpos(polypoints[3], pos3)
+	return poly
+
+class RhombusPart:
+	center = lambda prim: prim.center
+	point0 = lambda prim: prim[0]
+	point1 = lambda prim: prim[1]
+	point2 = lambda prim: prim[2]
+	point3 = lambda prim: prim[3]
+
+class Rhombus:
+	def __init__(self, corner0, corner1, corner2, corner3):
+		self.corners = [
+			_asvector(corner0),
+			_asvector(corner1),
+			_asvector(corner2),
+			_asvector(corner3),
+		]
+
+	def clone(self):
+		return Rhombus(*self.corners)
+
+	@property
+	def center(self):
+		return (self.corners[0] + self.corners[2]) / 2
+
+	def _getpart(self, part):
+		if callable(part):
+			return part(self)
+		if part in (None, 'center'):
+			return self.center
+		if part == 'corner0':
+			return self.corners[0]
+		if part == 'corner1':
+			return self.corners[1]
+		if part == 'corner2':
+			return self.corners[2]
+		if part == 'corner3':
+			return self.corners[3]
+		if isinstance(part, int):
+			return self.corners[part]
+		raise TypeError()
+
+	def move(self, translate):
+		translate = _asvector(translate)
+		for i in range(4):
+			self.corners[i] += translate
+
+	def movepartto(self, part, position):
+		self.move(_asvector(position) - self._getpart(part))
+
+	def rotateonpart(self, r, part=RhombusPart.center):
+		pivot = self._getpart(part)
+		matrix = tdu.Matrix()
+		matrix.rotate(0, 0, r, pivot=pivot)
+		for i in range(4):
+			self.corners[i] *= matrix
+
+	def offsetcorners(self, offset):
+		self.corners = [
+			self.corners[(i + offset) % 4]
+			for i in range(4)
+		]
+
+	def addtosop(self, sop):
+		return AddQuad(sop, *self.corners)
+
+	@classmethod
+	def create(cls, sidelength=1, angle=90):
+		anglerads = math.radians(angle)
+		width = sidelength * math.sqrt(2 - 2 * math.cos(anglerads))
+		height = sidelength * math.sqrt(2 + 2 * math.cos(anglerads))
+		pos0 = tdu.Vector(0, -height/2, 0)
+		pos1 = tdu.Vector(-width/2, 0, 0)
+		pos2 = tdu.Vector(0, height/2, 0)
+		pos3 = tdu.Vector(width/2, 0, 0)
+		return Rhombus(pos0, pos1, pos2, pos3)
+
+def _asvector(pt):
+	if hasattr(pt, 'point'):
+		pt = pt.point
+	if isinstance(pt, (list, tuple)):
+		if len(pt) == 2:
+			pt = list(pt) + [0]
+		return tdu.Vector(*pt)
+	return tdu.Vector(pt.x, pt.y, pt.z)
+
+def CreateRhombusPartParam(page, name, label=None):
+	par = page.appendMenu(name, label=label)[0]
+	par.menuNames = ['corner0', 'corner1', 'corner2', 'corner3', 'center']
+	par.menuLabels = ['Corner 0', 'Corner 1', 'Corner 2', 'Corner 3', 'Center']
+	return par
