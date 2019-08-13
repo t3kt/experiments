234acf27a38351756dc705026e40eb80aa858cc3
tekt <tekt@optexture.com>
2019-08-12 22:19:52 -0700

    support for triangle rendering in glsl threshold (incomplete)

--- glsl_threshold_calculate.glsl
+++ glsl_threshold_calculate.glsl
@@ -2,9 +2,39 @@ uniform int maxpoints;
 uniform vec2 distrange;
 uniform float requireDistinctGroups;
 
+/*
+	xyz: pt1.xyz
+	w: point 1 index / maxpoints
+		(i.e. the U position in the input data textures)
+*/
 layout(location = 0) out vec4 state1Out;
+
+/*
+	xyz: pt2.xyz
+	w: point 2 index / maxpoints
+		(i.e. the U position in the input data textures)
+*/
 layout(location = 1) out vec4 state2Out;
+
+/*
+	x: exists
+	y: absolute distance (for triangles, this is the average of that of its edges)
+	z: distance relative to range (same for this)
+	w: was evaluated (bool) (this is mostly just for debugging purposes to indicate which
+			pixels in the data are skipped over (since they'd be duplicates of other pixels)
+*/
 layout(location = 2) out vec4 attrsOut;
+
+#ifdef TRIANGLE_MODE
+
+/*
+	xyz: pt2.xyz
+	w: point 2 index / maxpoints
+		(i.e. the U position in the input data textures)
+*/
+layout(location = 3) out vec4 state3Out;
+
+#endif
 
 vec4 pointState(float u) {
 	return texture(sTD2DInputs[0], vec2(u, 0.0));
@@ -34,8 +64,12 @@ vec4 map(vec4 value, vec4 inMin, vec4 inMax, vec4 outMin, vec4 outMax) {
   return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
 }
 
+bool inRange(float d) {
+	return d >= distrange.x && d <= distrange.y;
+}
+
 bool isMatch(float d, float group1, float group2) {
-	if (d < distrange.x || d > distrange.y) {
+	if (!inRange(d)) {
 		return false;
 	}
 	if (requireDistinctGroups > 0.5 && group1 == group2) {
@@ -53,30 +87,26 @@ void main()
 	float group1 = pointGroup(vUV.s);
 	float group2 = pointGroup(vUV.t);
 	
-	vec4 state1 = vec4(0.0);
-	vec4 state2 = vec4(0.0);
-	vec4 attrs = vec4(0.0);
+	vec4 state1 = vec4(0);
+	vec4 state2 = vec4(0);
+	vec4 attrs = vec4(0);
 	
     //if (point1.x < 0.5) {
     //	state1 = vec4(0.0,1.0,0.0,1.0);
     //	state2 = vec4(0.0,0.0,1.0,1.0);
     //	attrs = vec4(-84.5,1.0,1.0,1.0);
     //}
+	#ifndef TRIANGLE_MODE
 	if (vUV.s < vUV.t &&
 	 point1.a > 0 && point2.a > 0) {
 		float d = distance(point1.xyz, point2.xyz);
 		if (!isMatch(d, group1, group2)) {
-			state1 = vec4(0.0);
-			state2 = vec4(0.0);
-			attrs = vec4(0.0, 0.0, 0.0, 1.0);
+			attrs = vec4(0, 0, 0, 1);
 		} else {
 			state1 = vec4(point1.xyz, vUV.s);
 			state2 = vec4(point2.xyz, vUV.t);
-			float reldist;
-			//reldist = map(d, distrange.x, distrange.y, 0.0, 1.0);
-			reldist = smoothstep(distrange.x, distrange.y, d);
 			float absdist = d;
-			//d = 0.0;
+			float reldist = smoothstep(distrange.x, distrange.y, absdist);
 			attrs = vec4(
 				/*exists*/ 1.0,
 				/*absdist*/ absdist,
@@ -84,8 +114,41 @@ void main()
 				1.0);
 		}
 	}
+	#else
+	vec4 point3 = pointState(vUV.p);
+	float group3 = pointGroup(vUV.p);
+	vec4 state3 = vec4(0);
+	if (vUV.s < vUV.t && vUV.t < vUV.p && point1.a > 0 && point2.a > 0 && point3.a > 0) {
+		float d12 = distance(point1.xyz, point2.xyz);
+		float d23 = distance(point2.xyz, point3.xyz);
+		float d31 = distance(point3.xyz, point1.xyz);
+		if (!isMatch(d12, group1, group2) ||
+				!isMatch(d23, group2, group3) ||
+				!isMatch(d31, group3, group1)) {
+			attrs = vec4(0, 0, 0, 1);
+		} else {
+			state1 = vec4(point1.xyz, vUV.s);
+			state2 = vec4(point2.xyz, vUV.t);
+			state3 = vec4(point3.xyz, vUV.p);
+			float reldist12 = smoothstep(distrange.x, distrange.y, d12);
+			float reldist23 = smoothstep(distrange.x, distrange.y, d23);
+			float reldist31 = smoothstep(distrange.x, distrange.y, d31);
+			float absdist = (d12 + d23 + d31) / 3.0;
+			float reldist = smoothstep(distrange.x, distrange.y, absdist);
+			attrs = vec4(
+				/*exists*/ 1.0,
+				/*absdist*/ absdist,
+				/*reldist*/ reldist,
+				1.0);
+		}
+	}
+	#endif
 	
-    state1Out = TDOutputSwizzle(state1);
-    state2Out = TDOutputSwizzle(state2);
-    attrsOut = TDOutputSwizzle(attrs);
+  state1Out = TDOutputSwizzle(state1);
+  state2Out = TDOutputSwizzle(state2);
+  attrsOut = TDOutputSwizzle(attrs);
+
+	#ifdef TRIANGLE_MODE
+  state3Out = TDOutputSwizzle(state3);
+	#endif
 }
--- glsl_threshold_render_vert.glsl
+++ glsl_threshold_render_vert.glsl
@@ -1,11 +1,52 @@
 uniform int maxpoints;
 uniform vec4 uColor = vec4(1);
-uniform sampler2D sPointState1;
-uniform sampler2D sPointState2;
-uniform sampler2D sLineAttrs;
+
+#ifdef TRIANGLE_MODE
+#define SAMPLER_TYPE sampler3D
+#else
+#define SAMPLER_TYPE sampler2D
+#endif
+
+/*
+	xyz: pt1.xyz
+	w: point 1 index / maxpoints
+		(i.e. the U position in the input data textures)
+*/
+uniform SAMPLER_TYPE sPointState1;
+
+/*
+	xyz: pt2.xyz
+	w: point 2 index / maxpoints
+		(i.e. the U position in the input data textures)
+*/
+uniform SAMPLER_TYPE sPointState2;
+
+/*
+	x: exists
+	y: absolute distance (for triangles, this is the average of that of its edges)
+	z: distance relative to range (same for this)
+	w: (nothing)
+*/
+uniform SAMPLER_TYPE sAttrs;
+
 uniform sampler2D sPointColors;
 
+#ifdef TRIANGLE_MODE
+
+/*
+	xyz: pt3.xyz
+	w: point 3 index / maxpoints
+		(i.e. the U position in the input data textures)
+*/
+uniform SAMPLER_TYPE sPointState3;
+
+in int vertNum; // comes in as 0 .. 2
+
+#else
+
 in float lineEnd; // comes in as -1 .. 1
+
+#endif
 
 out Vertex {
 	vec4 color;
@@ -14,17 +55,27 @@ out Vertex {
 void main()
 {
 	int inst = TDInstanceID();
+	#ifndef TRIANGLE_MODE
 	ivec2 dataCoord = ivec2(
 		inst / maxpoints,
 		inst % maxpoints);
-	vec4 lineAttrVals = texelFetch(sLineAttrs, dataCoord, 0);
+	#else
+	ivec3 dataCoord;
+//	= ivec3(
+//		(inst % (maxpoints * maxpoints)) % maxpoints
+//		);
+// TODO: STUFF!!!
+	#endif
+	vec4 attrVals = texelFetch(sAttrs, dataCoord, 0);
 	vec3 pos;
-	if (!bool(lineAttrVals.r) || dataCoord.y <= dataCoord.x) {
+	if (!bool(attrVals.r) || dataCoord.y <= dataCoord.x) {
 		oVert.color = vec4(0);
 		pos = vec3(0);
 	} else {
 		vec4 pointState1 = texelFetch(sPointState1, dataCoord, 0);
 		vec4 pointState2 = texelFetch(sPointState2, dataCoord, 0);
+
+		#ifndef TRIANGLE_MODE
 //		rotation *=  -lineEnd;
 //		mat3 rotation = TDRotateToVector(
 //			pointState2.xyz - pointState1.xyz,
@@ -47,9 +98,13 @@ void main()
 		pos = pointStateVals.xyz;
 		#endif
 		oVert.color = texture(sPointColors, vec2(pointStateVals.w, 0)) * uColor;
-		float closeness = 1.0 - lineAttrVals.b;
+		float closeness = 1.0 - attrVals.b;
 		closeness = pow(closeness, 0.1);
 		oVert.color *= closeness;
+
+		#else
+// TODO: STUFF!!!
+		#endif
 	}
 
 	vec4 worldSpacePos = TDDeform(pos);
