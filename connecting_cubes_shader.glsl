11214c647cc8b8b089e36af9d7dd94f26a0b4777
tekt <t3kt@optexture.com>
2020-03-07 18:36:27 -0800

    cube things

--- connecting_cubes_shader.glsl
+++ connecting_cubes_shader.glsl
@@ -73,6 +73,12 @@ float sdCapsule( vec3 p,
   return length( pa - ba*h ) - r;
 }
 
+float sdCappedCylinder( vec3 p, float h, float r )
+{
+  vec2 d = abs(vec2(length(p.xz),p.y)) - vec2(h,r);
+  return min(max(d.x,d.y),0.0) + length(max(d,0.0));
+}
+
 float opSmoothUnion( float d1, float d2, float k) {
     float h = clamp( 0.5 + 0.5*(d2-d1)/k, 0.0, 1.0 );
     return mix( d2, d1, h ) - k*h*(1.0-h); }
